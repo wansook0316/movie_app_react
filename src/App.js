1622b0812d0d17f86e51e7b5537aaf6fd9188988
wansook <wansook0316@gmail.com>
2020-03-07 17:01:42 +0900

    #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -7,7 +8,14 @@ class App extends React.Component {
     movies: []
   };
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
 
   componentWillMount() {
@@ -15,8 +23,23 @@ class App extends React.Component {
   }
   render() {
     // this.state 객체 안에 있는 멤버변수 isLoading을 찾아서 그것의 값을 자바스크립트 변수 isLoading 안에 넣어라
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "we are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.poster}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
