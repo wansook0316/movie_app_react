60c68d58c2ea46694df565d8e8110852a0a960d9
wansook <wansook0316@gmail.com>
2020-03-07 23:08:05 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation /> {/* 이 component는 HashRouter 안에 있어야 한다. */}
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
       {/* <Route path="/home">
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Link를 사용하기 위해서는 해당 React Component가 HashRoute component 안에 있어야 한다.
+function Navigation() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
+
+export default Navigation;
