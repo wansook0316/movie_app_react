343aa4f0e2bd6ca0f9988179c93c9f6a10715d4a
wansook <wansook0316@gmail.com>
2020-03-07 19:11:58 +0900

    Updates

---
+++ precache-manifest.17ecbd468527d86c382f766536a6a89e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a184546e274cd9fcdebc28fb94b6085",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "848282d04eadd17d0f0c",
+    "url": "/movie_app_react/static/css/main.08292def.chunk.css"
+  },
+  {
+    "revision": "036b944ee911e059105f",
+    "url": "/movie_app_react/static/js/2.f30a03c3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_react/static/js/2.f30a03c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848282d04eadd17d0f0c",
+    "url": "/movie_app_react/static/js/main.6bdf3193.chunk.js"
+  },
+  {
+    "revision": "9678b2b7e32aa6f11619",
+    "url": "/movie_app_react/static/js/runtime-main.4c685b15.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/movie_app_react/precache-manifest.17ecbd468527d86c382f766536a6a89e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app_react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
