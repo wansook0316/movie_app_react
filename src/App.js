ff70543e64ba6de52fd9b0c9ee80b2c255776991
wansook <wansook0316@gmail.com>
2020-03-07 17:53:59 +0900

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -18,27 +19,33 @@ class App extends React.Component {
     this.setState({ movies, isLoading: false });
   };
 
-  componentWillMount() {
+  componentDidMount() {
     this.getMovies();
   }
   render() {
     // this.state 객체 안에 있는 멤버변수 isLoading을 찾아서 그것의 값을 자바스크립트 변수 isLoading 안에 넣어라
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
-                poster={movie.poster}
+                poster={movie.large_cover_image}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
