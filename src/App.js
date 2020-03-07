3e76ae96f313d36019cd299ee2919356f2cbe477
wansook <wansook0316@gmail.com>
2020-03-08 00:05:43 +0900

    #6.3 Sharing Props Between Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 function App() {
@@ -10,15 +11,7 @@ function App() {
       <Navigation /> {/* 이 component는 HashRouter 안에 있어야 한다. */}
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      {/* <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/introduction">
-        <h1>Introduction</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route> */}
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 // Link를 사용하기 위해서는 해당 React Component가 HashRoute component 안에 있어야 한다.
 function Navigation() {
   return (
-    <div>
+    <div className="nav">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
